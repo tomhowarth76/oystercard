c6a1193f9481472b13b0e0fa1e3a7d1447dfa8fe
tomhowarth76 <tomhowarth@mac.com>
2019-11-07 15:40:16 +0000

    moving on to q11

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,5 +1,6 @@
 class Oystercard
 attr_reader :balance, :status
+
 MAXBALANCE = 90
 MINBALANCE = 1
 
@@ -7,7 +8,6 @@ MINBALANCE = 1
     @balance = 0
     @status = false
   end
-
   def topup(amount)
     fail "Over limit of £#{MAXBALANCE}" unless @balance < MAXBALANCE
     @balance += amount
@@ -16,12 +16,14 @@ MINBALANCE = 1
   def deduct(fare)
     @balance -= fare
   end
+  private :deduct
 
   def touch_in
    fail "Balance must be over £1" unless @balance > MINBALANCE
     @status = true
   end
   def touch_out
+    deduct(1)
     @status = false
   end
   def in_journey?
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,50 +1,55 @@
 require './lib/oystercard'
 
 describe Oystercard do
+  let (:oyster) {Oystercard.new}
   it 'checks that the oystercard has a balance' do
-    oyster = Oystercard.new
+  #  oyster = Oystercard.new
     expect(oyster.balance).to eq(0)
   end
 
   it 'tops up balance of oystercard' do
-    oyster = Oystercard.new
+  #  oyster = Oystercard.new
     amount = 10
     expect(oyster.topup(amount)).to eq(amount)
   end
 
   it 'raises an error if topped up beyond £90' do
-    oyster = Oystercard.new
+  #  oyster = Oystercard.new
     amount = 10
     9.times { oyster.topup(amount) }
     expect { oyster.topup(amount) }.to raise_error 'Over limit of £90'
   end
-  it 'deducts a fare from the oyster balance' do
-    oyster = Oystercard.new
-    amount = 10
-    9.times { oyster.topup(amount) }
-    fare = 10
-    expect(oyster.deduct(fare)).to eq(80)
-  end
+  # it 'deducts a fare from the oyster balance' do
+  # #  oyster = Oystercard.new
+  #   amount = 10
+  #   9.times { oyster.topup(amount) }
+  #   fare = 10
+  #   expect(oyster.deduct(fare)).to eq(80)
+  # end
 
   it 'touches in if over minimum fare' do
-    oyster = Oystercard.new
+#    oyster = Oystercard.new
     oyster.topup(10)
     expect(oyster.touch_in).to be(true)
   end
 
   it 'touches out' do
-    oyster = Oystercard.new
+#    oyster = Oystercard.new
     expect(oyster.touch_out).to be(false)
   end
   it 'knows it is in journey' do
-     oyster = Oystercard.new
+  #   oyster = Oystercard.new
       oyster.topup(10)
       oyster.touch_in
      expect(oyster.in_journey?).to be(true)
   end
   it 'raises an error if touched in below minimum fare' do
-    oyster = Oystercard.new
+#    oyster = Oystercard.new
     expect { oyster.touch_in }.to raise_error 'Balance must be over £1'
   end
-  it 'reduces the balance by the minimum '
+  it 'reduces the balance by the fare' do
+    fare = 1
+    oyster.topup(10)
+    expect { oyster.touch_out }.to change { oyster.balance }.by(-fare)
+  end
 end
